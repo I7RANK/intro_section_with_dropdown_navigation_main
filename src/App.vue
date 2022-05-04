79cfb7b0353ac682f2a68ff55ba0d01362d68967
I7RANK <frafgh2000@hotmail.com>
2022-05-04 17:22:38 -0500

    init project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<template>
+  <Header />
+
+  <main>
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+</style>
