44a1520214b65560ecc5a1be553558b7ec4663fa
I7RANK <frafgh2000@hotmail.com>
2022-05-04 22:41:32 -0500

    Adding dropdown sub menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -21,17 +21,26 @@ export default {
           { name: "Calendar", icon: iconCalendar },
           { name: "Reminders", icon: iconReminders },
           { name: "Planing", icon: iconPlanning }
-        ] },
+        ],
+        toggleSubItem: false },
         { name: "Company", subItems: [
           { name: "History", icon: "" },
           { name: "Out Team", icon: "" },
           { name: "Blog", icon: "" }
-        ] },
-        { name: "Careers", subItems: [] },
-        { name: "About", subItems: [] },
+        ],
+        toggleSubItem: false },
+        { name: "Careers", subItems: [], toggleSubItem: false },
+        { name: "About", subItems: [], toggleSubItem: false },
       ]
     };
   },
+  methods: {
+    handleToggleSubItem (item) {
+      if (!item.subItems.length) return;
+
+      item.toggleSubItem = !item.toggleSubItem;
+    }
+  }
 };
 </script>
 
@@ -40,8 +49,22 @@ export default {
     <nav class="main-menu">
       <ul>
         <li :key="item.name" v-for="item in navItems">
-          {{ item.name }}
-          <img v-if="item.subItems.length" :src="iconArrowDown" alt="">
+          <span class="item-name" @click="this.handleToggleSubItem(item)">
+            {{ item.name }}
+            <img v-if="item.subItems.length && !item.toggleSubItem" :src="iconArrowDown" alt="">
+            <img v-else-if="item.subItems.length" :src="iconArrowUp" alt="">
+          </span>
+
+          <div v-if="item.toggleSubItem" class="sub-items-dropdown">
+            <ul>
+              <li class="sub-item-name" :key="subItem.name" v-for="subItem in item.subItems">
+                <span>
+                  <img v-if="subItem.icon" :src="subItem.icon" alt="">
+                  {{ subItem.name }}
+                </span>
+              </li>
+            </ul>
+          </div>
         </li>
       </ul>
     </nav>
@@ -59,21 +82,44 @@ export default {
   flex-direction: column;
   padding: 0 20px;
   color: var(--medium-gray);
+  font-size: .9rem;
+}
+
+.navbar-content li {
+  min-height: 30px;
 }
 
 .main-menu ul {
   padding: 0;
 }
 
-.main-menu ul li {
-  display: flex;
-  align-items: center;
-  list-style: none;
-  height: 45px;
+.main-menu ul li span {
+  cursor: pointer;
+  user-select: none;
 }
 
-.main-menu ul li img {
+.navbar-content li {
+  list-style: none;
+}
+
+.item-name img {
   margin: 0 15px;
+}
+
+.sub-items-dropdown {
+  margin: 20px;
+  transform-origin: left;
+  animation: subMenuAnimation .3s ease;
+}
+
+.sub-items-dropdown .sub-item-name {
+  min-height: 30px;
+}
+
+.sub-item-name img {
+  height: 16px;
+  width: 16px;
+  margin: 0 7px 0 0;
 }
 
 .sign-button-content {
@@ -92,5 +138,10 @@ export default {
 
 .sign-button-content .btn-sign-out {
   border: solid 2px var(--medium-gray);
+}
+
+@keyframes subMenuAnimation {
+  from {transform: scale(0);}
+  to {transform: scale(1);}
 }
 </style>
